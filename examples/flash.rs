d8a1ecc38e4d2e29c2e9104154854dfe45798580
rahul-thakoor <rahul.thakoor@gmail.com>
2018-06-09 22:57:37 +0400

    onoff and button

---
+++ examples/flash.rs
@@ -0,0 +1,25 @@
+extern crate rust_gpiozero;
+use rust_gpiozero::*;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+
+// Tell the Pi which GPIO pin you are using
+
+
+loop{
+    // Make the led switch on
+    
+
+    // Let the LED stay on for one second
+    
+
+    // Make the led switch off
+   
+
+    // Let the LED stay off for one second
+    
+    
+    }
+}
---
+++ examples/onoff.rs
@@ -0,0 +1,11 @@
+extern crate rust_gpiozero;
+use rust_gpiozero::*;
+
+fn main() {
+
+// Tell the Pi which GPIO pin you are using
+
+
+// Make the led switch on/off
+
+}
